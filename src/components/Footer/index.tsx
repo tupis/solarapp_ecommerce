e6503a308bf0bfcb22e06b29fea3d7d776ce3431
João Tupinambá <joaoh.tupinamba@gmail.com>
2024-09-08 02:17:20 -0300

    feat: footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2024 SolarApp. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
