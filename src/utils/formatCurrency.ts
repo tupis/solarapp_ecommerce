74bfeabcd4a92ea9a7fdf6d5b4081d00be6c74b7
João Tupinambá <joaoh.tupinamba@gmail.com>
2024-09-09 00:08:47 -0300

    fix: using other method to reurn currency

--- src/utils/formatCurrency.ts
+++ src/utils/formatCurrency.ts
@@ -1,14 +1,11 @@
 export function formatCurrencyString(value: string) {
-  const numericValue = value.replace(/\D/g, "");
+  if (!value) return "R$ 0,00";
 
-  const numberValue = parseInt(numericValue, 10) || 0;
+  const numericValue = value.padStart(3, "0");
+  const realCurrency = numericValue.slice(0, -2);
+  const cents = numericValue.slice(-2);
 
-  const amount = numberValue / 100;
-
-  return amount.toLocaleString("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-  });
+  return `R$ ${parseInt(realCurrency, 10)},${cents}`;
 }
 
 export function formatCurrencyNumber(value: number) {
